cfd0e560e219917177ea893670a2fd25d66261b9
Alexey Schcennikov <hopefortomorrow@bk.ru>
2024-04-25 01:58:51 +0300

    added bootstrap5 and small fixes

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,20 @@
 from django.contrib.auth import views as auth_view
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.urls import path
 
 from . import views
 from .forms import RussianLoginForm
+
+
+class CustomLoginView(auth_view.LoginView):
+    authentication_form = RussianLoginForm
+    template_name = 'users/login.html'
+    redirect_authenticated_user = False
+
+    def get_success_url(self):
+        return reverse_lazy('profile')
 
 
 def custom_logout(request):
@@ -18,8 +27,9 @@ def custom_logout(request):
 
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
-    path('login/', auth_view.LoginView.as_view(authentication_form=RussianLoginForm,
-                                               template_name='users/login.html',
-                                               redirect_authenticated_user=False), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(next_page=''), name='logout')
+    path('login/', CustomLoginView.as_view(authentication_form=RussianLoginForm,
+                                           template_name='users/login.html',
+                                           redirect_authenticated_user=False), name='login'),
+    path('logout/', custom_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
